d2f7450deca112891499c650fd8bf10979c0d6de
Berlandro <bryangodlive0@gmail.com>
2024-05-10 14:14:34 +0700

    feat: fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import HomePage from "./pages/home"
-import OrdersPage from "./pages/orders"
+
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/orders" element={<OrdersPage/>}/>
-      </Routes>
-    </BrowserRouter>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import OrdersPage from './pages/orders'
+import HomePage from './pages/home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/orders" element={<OrdersPage/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )

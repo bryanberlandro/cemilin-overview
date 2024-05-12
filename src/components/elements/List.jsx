9a4e7617126b1ecba31f6ae2438f96c6afd20be7
Berlandro <bryangodlive0@gmail.com>
2024-05-12 11:24:07 +0700

    style : change style at sidebar

--- src/components/elements/List.jsx
+++ src/components/elements/List.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 /* eslint-disable react/prop-types */
 export function List({name, linkTo, onClick}){
+    const location = useLocation()
+    
     return(
         <>
         <Link onClick={onClick} to={linkTo}>
-            <li className="py-4 hover:bg-violet-600 px-4 rounded-md cursor-pointer">
+            <li className={`py-3  hover:bg-violet-600 ${location.pathname == linkTo ? "bg-violet-600 text-white" : "text-neutral-200"} px-4 rounded-md cursor-pointer`}>
                 {name}
             </li>
         </Link>
--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -21,13 +21,24 @@ export function Sidebar({showSidebar, closeSidebar}){
     return(
         <>
             <div onClick={closeSidebar} className={`transition duration-500 fixed ${showSidebar ? "flex" : "hidden"} w-full h-dvh backdrop-blur-[1px] bg-neutral-100 bg-opacity-15 z-[99] ${transition ? "opacity-100" : "opacity-0"}`}></div>
-            <div className={`fixed left-0 top-0 h-dvh z-[999] w-48 px-2 bg-violet-500 transition-all duration-500 pt-nav text-white ${showSidebar ? 'translate-x-0' : '-translate-x-96'}`}>
+            <div className={`fixed left-0 top-0 h-dvh z-[999] w-52 px-2 bg-violet-500 shadow-soft transition-all duration-500 pt-nav text-white ${showSidebar ? 'translate-x-0' : '-translate-x-96'}`}>
                 <ul>
                     <List name={"Home"} linkTo={"/"} onClick={closeSidebar}/>
-                    <List name={"Dasbhoard"} linkTo={"/dashboard"} onClick={closeSidebar}/>
-                    <List name={"Orders"} linkTo={"/orders"} onClick={closeSidebar}/>
-                    <List name={"Products"} linkTo={"/products"} onClick={closeSidebar}/>
-                    <List name={"To Do"} linkTo={"/todo"} onClick={closeSidebar}/>
+                    <div className="my-4 px-4">
+                        <h1 className="font-bold">Daily Operations</h1>
+                        <div className="mt-1">
+                        <List name={"Orders"} linkTo={"/orders"} onClick={closeSidebar}/>
+                        <List name={"Tasks"} linkTo={"/tasks"} onClick={closeSidebar}/>
+                        </div>
+                    </div>
+                    <div className="my-4 px-4">
+                        <h1 className="font-bold">Management</h1>
+                        <div className="mt-1">
+                            <List name={"Dasbhoard"} linkTo={"/dashboard"} onClick={closeSidebar}/>
+                            <List name={"Products"} linkTo={"/products"} onClick={closeSidebar}/>
+                        </div>
+                    </div>
+
                 </ul>
             </div>
         </>

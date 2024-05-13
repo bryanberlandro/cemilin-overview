a4268881e54638c93099bfc617f7a22b7bdb45c3
Berlandro <bryangodlive0@gmail.com>
2024-05-13 10:27:34 +0700

    feat: order details add orders page

--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FaTrash } from "react-icons/fa";
+import { FaChevronDown, FaInfoCircle, FaTrash } from "react-icons/fa";
 import { Alert } from "../components/fragments/Alert";
 import { Navbar } from "../components/layouts/Navbar";
 import { Form } from "../components/layouts/orders/Form";
@@ -10,17 +10,27 @@ import { StatusBtn } from "../components/elements/StatusBtn";
 import { Rupiah } from "../utils/Rupiah";
 import { Loader } from "../components/fragments/Loader";
 import axios from "axios";
+import gsap from "gsap";
 
 const OrdersPage = () => {
     const [success, setSuccess] = useState(false)
     const {data, isLoading, isError} = useFetch("https://cemilin-api.vercel.app/products")
     const [selectedProduct, setSelectedProduct] = useState([]);
+    const [showDetails, setShowDetails] = useState(false);
     const [totalPrice, setTotalPrice] = useState(0)
     const [totalChange, setTotalChange] = useState(0)
     const [status, setStatus] = useState("")
     const [paid, setPaid] = useState(0)
     const [name, setName] = useState("")
     const [purchasedDate, setPurchasedDate] = useState("");
+
+    useEffect(() => {
+        if(selectedProduct.length > 0){
+            gsap.to("#floatDetails", { y: 0, duration: 1, ease: "expo.inOut" });
+            return
+        }
+        gsap.to("#floatDetails", { y: 1000, duration: 3, delay: 1, ease: "expo.inOut" });
+    }, [selectedProduct])
 
     useEffect(() => {
         if(selectedProduct){
@@ -97,17 +107,24 @@ const OrdersPage = () => {
         } catch(err){
             console.log(err)
         }
+        const timeout = setTimeout(() => {
+            setSuccess(false)
+        }, 3000)
+        return () => clearTimeout(timeout);
     }
 
     function handleStatus(e){
         setStatus(e)
     }
+    function handleShowDetails(){
+        setShowDetails(!showDetails)
+    }
 
     return(
         <>
         <Navbar/>
-        <div className="pt-nav pb-10 px-[5%]">
-            <div className="bg-white px-5 rounded-lg w-full py-6">
+        <div className="pt-nav pb-10 px-[1%]">
+            <div className={`bg-white px-5 rounded-lg w-full py-6 ${selectedProduct.length > 0 ? "mb-44" : ""}`}>
                 <div className="text-lg font-bold text-violet-500 flex justify-between items-center">
                     <h1>Input Order</h1>
                     <FaTrash/>
@@ -184,43 +201,57 @@ const OrdersPage = () => {
                                 </div>
                             </div>
                         </form>
-                        <div className="mt-6">
-                            <h1 className="font-medium">Order Details</h1>
-                            <div className="text-neutral-500 text-sm mt-2">
-                                <div className="flex justify-between items-center">
-                                    <p>Name</p>
-                                    <p>Aldio</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div id="floatDetails" className="fixed bottom-0 rounded-t-2xl shadow-soft px-[5%] py-5 w-full bg-white left-0">
+                        <div onClick={handleShowDetails} className="flex items-center gap-2 text-sm font-medium text-violet-500">
+                        <FaInfoCircle/>
+                        <h1>Click for details</h1>
+                        </div>
+                        <div className={`text-neutral-500 text-sm mt-2 flex-col gap-1 ${showDetails ? "flex" : "hidden"}`}>
+                            <div className="flex justify-between items-center">
+                                <p>Name</p>
+                                <p>{name ? name : ""}</p>
+                            </div>
+                            <div className="flex justify-between items-start">
+                                <p>Item</p>
+                                <div className="flex flex-col justify-end text-right">
+                                    {
+                                        selectedProduct.length > 0 ? 
+                                            selectedProduct.map(prod => (
+                                                <p key={prod._id}>{prod.name} {prod.pieces} pcs</p>
+                                            ))
+                                        : 
+                                        <p>0</p>
+                                    }
                                 </div>
-                                <div className="flex justify-between items-center">
-                                    <p>Item</p>
-                                    <p>Cireng 5 pcs, Lumpia Telur 4 pcs</p>
-                                </div>
-                                <div className="flex justify-between items-center">
+                            </div>
+                            <div className="flex justify-between">
                                     <p>Total Paid</p>
-                                    <p>Rp 10.000</p>
-                                </div>
+                                    <p>{paid ? Rupiah(paid) : "Rp 0"}</p>
+                            </div>
                                 <div className="flex justify-between items-center">
                                     <p>Total Change</p>
-                                    <p>Rp 0</p>
+                                    <p>{totalChange ? Rupiah(totalChange) : "Rp 0"}</p>
                                 </div>
                                 <div className="flex justify-between items-center">
                                     <p>Purchase Date</p>
-                                    <p>24-05-2024</p>
+                                    <p>{purchasedDate ? purchasedDate : "YYYY-MM-DD"}</p>
                                 </div>
-                                <div className="flex text-lg text-black font-medium mt-1 justify-between items-center">
-                                    <p>Total Price</p>
-                                    <p>{Rupiah(totalPrice)}</p>
-                                </div>
-                            </div>
                         </div>
-                        <button className="w-full py-3 bg-violet-400 rounded-lg text-white mt-8" onClick={handleSendOrder}>Input Order</button>
-                    </div>
-                </div>
+                        <div className="flex text-lg text-black font-medium mt-4 justify-between items-center">
+                            <p>Total Price</p>
+                            <p>{Rupiah(totalPrice)}</p>
+                        </div>
+                    <button className="w-full py-3 bg-violet-400 rounded-lg text-white mt-4" onClick={handleSendOrder}>Input Order</button>
             </div>
             <Alert success={success}/>
         </div>
         </>
     )
 }
+
 
 export default OrdersPage;
